b271d664d197368f96c9e54dadbf1ae7188d8f30
Dharmik Santhosh Raghav <70014740+dharmiksanthosh@users.noreply.github.com>
2021-01-22 13:47:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Search from './screens/Search';
@@ -17,7 +17,33 @@ export default class App extends React.Component {
 const TabNavigator = createBottomTabNavigator({
   Transaction: {screen: TransactionScreen},
   Search: {screen: Search}
-})
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) =>({
+      tabBarIcon: () =>{
+        const routeName = navigation.state.routeName;
+
+        if (routeName === 'Transaction') {
+          return (
+            <Image style={{width:40,height:40}} source={require('./assets/book.png')}/>
+          )
+        } else if (routeName === 'Search') {
+          return (
+            <Image style={{width:40,height:40}} source={require('./assets/search.png')}/>
+          )
+        }
+      },
+      tabBarOptions: {
+        activeTintColor:'black',
+        activeBackgroundColor:'#ccc',
+        style: {
+            backgroundColor: '#f7f7f7',
+            height: 70
+        },
+      }
+    })
+  }
+)
 const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
